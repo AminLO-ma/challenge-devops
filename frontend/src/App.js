090b1182e616ac5b1b5ff99012402399b9ed10f8
Khadija Azdad <mohamedamine.elouahbi@gmail.comm>
2025-04-14 09:58:59 +0200

    Add fullstack project structure:  React frontend and Express backend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Quiz WebApp</h1>
+        <p>A web application for creating and answering quizzes</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
